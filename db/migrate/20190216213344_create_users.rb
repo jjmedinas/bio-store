da58fb77158541ffba254593ee5c1e405e04f059
jjmedinas <juan.ms@outlook.com>
2019-02-16 18:19:54 -0500

    Create scaffold user

---
+++ db/migrate/20190216213344_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :nickname
+      t.string :email
+      t.string :password_digest
+      t.string :role, default: 'buyer'
+      t.string :state, default: 'active'
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
